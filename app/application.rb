ead479d5a8d40aaf626517ba1e5cebd74db1f33f
Mark May <mamark90@gmail.com>
2020-08-08 00:25:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     t = Time.now
-    hour =  t.strftime("%H").to_i
+    hour =  t.strftime("%H%M").to_i
 
     resp.write "#{hour}\n"
 
